0dc78ab81df23748c92715657583e19a0fc0f52a
Marvin <dont@show.myemailS>
2022-09-16 21:09:42 -0300

    Adds new board model. Kickstarts the board model development.

---
+++ app/Models/BoardModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BoardModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'boards';
+
+    protected $primaryKey = 'uri';
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    public function getBoardViewData($uri, $page) {
+
+    }
+
+    public function updateBoard($uri) {
+        
+    }
+
+    public function createBoard($options) {
+        
+    }
+
+    public function deleteBoard($uri) {
+        
+    }
+}
