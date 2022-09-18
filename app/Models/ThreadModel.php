3ff5c950324e16fda067b90a72b6fb4bebf347d6
Marvin <dont@show.myemailS>
2022-09-18 18:10:49 -0300

    Prepares the functions that will retrieve all thread data per board page.

--- app/Models/ThreadModel.php
+++ app/Models/ThreadModel.php
@@ -29,4 +29,31 @@ class ThreadModel extends PostModelParent
     function updateThread ($newBody, $newTitle, $threadId) {
         
     }
+
+    /**
+     *  Returns an array of the desired threads 
+     *  with all attributes (locked, highlighted, etc.)
+     */
+
+    private function getNonPinnedThreadsPerPage($uri, $pagination) { 
+        // laravel automatically detects the 'page' input of the current request
+        return $self::where('board_uri', $uri)
+                    ->where('is_pinned', false)
+                    ->orderBy('created_at')
+                    ->paginate($pagination)
+                    ->get();
+    }
+
+    private function getPinnedThreadsPerPage($uri, $pagination) { 
+        // laravel automatically detects the 'page' input of the current request
+        return $self::where('board_uri', $uri)
+                    ->where('is_pinned', true)
+                    ->orderBy('last_pinned_at')
+                    ->paginate($pagination)
+                    ->get();
+    }
+
+    public function getAllThreads($uri, $pagination) {
+        return getPinnedThreadsPerPage($uri, $pagination) + getNonPinnedThreadsPerPage($uri, $pagination);
+    }
 }
