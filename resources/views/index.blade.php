172473f77b33676a836871122e75759b369a917b
Marvin <dont@show.myemailS>
2022-12-06 17:30:20 -0300

    Fixes a lot of glitches in the index view.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,31 +31,40 @@
 </table>
 
 <img src="a.jpg" class="global-banner">  {{-- what if this was a featured image from the booru? --}} 
-<div class="index-boards">Main boards</div>
+<div class="index-boards">Featured boards</div>
 <div class="index-boardlist">
     @if(isset($index['boards']))
     @foreach($index['boards'] as $board)
     <a href="/boards/{{ $board['uri'] }}" class="index-board-hyperlink">$board['name']</a>
     @endforeach
+    @else
+    <a class="index-board-hyperlink">No boards featured</a>
     @endif {{-- todo: if no boards, display: "no boards yet" --}}
 </div>  
 <hr class="board-separator">
-<div class="index-thread-intro">Threads</div>
+<div class="index-thread-intro">Hot threads</div>
 <ul class="index-thread-ul"> {{-- this is where "hot" threads are supposed to be --}}
     @if(isset($index['threads']))
     @foreach($index['threads'] as $thread)
     <li><a href="/boards/{{ $thread['board'] }}/{{ $thread['id'] }}">{{ $thread['title'] }}.</a></li>
     @endforeach
+    @else
+    <li style="color: grey;">No threads yet. Only the silence.</li>
     @endif {{-- todo: no featured threads, display: "nothing to see here yet" --}}
 </ul> 
 
 <table class="index-booru__table"> 
 <tr><th class="index-booru-table__header">Booru</th></tr>
-<tr><td class="index-booru-table__body">
+<tr><td class="index-booru-table__body"> 
+  {{-- 
+    maybe dump the "featured img" thing and turn this into the list of all public boards 
+  --}}
   @if(isset($booru['featured_images']))
   @foreach($booru['featured_images'] as $image)
   <img class="index-booru-table__img" src="/images/{{ $image['uri'] }}">
   @endforeach
+  @else
+  <div style="margin: auto;">Poster images will appear here</div>
   @endif
 </td></tr>
 </table>
@@ -67,7 +76,6 @@
 <div class="box-vp box2">
 <div class="box2-wrapper">
   <div class="hot-section">
-    <div class="index-news-section">
       <div class="index-news-section__wrapper">
         <div class="index-news-section__intro">Latest news</div>
         <table class="news-table"> 
@@ -78,26 +86,19 @@
             <td class="news-title"><a href="/news/{{ $news['uri'] }}">{{ $news['title']}}</a><td>
             <td class="news-date" data-news-date="$news['date_unix_time']" {{-- for javascripts relative time conversion --}}>{{ $news['date_human_readable'] }}<td>
             @endforeach
+            @else
+            <td class="news-title">No news yet.<td>
+            <td class="news-date">(no date)<td>
+
             @endif
           </tr>
           </tbody>
         </table>
       </div>
-    </div>
-    <div class="hottest-threads">
-      <div class="hottest-threads-wrapper">
-        <div class="hottest-threads-text">Hottest Threads</div>
-        <table class="hot-threads-table">
-          <tr>
-            <td class="hot-thread-index">I created this thread and now it's full of replies</td> {{-- we dont need this anymore --}}
-          </tr>
-        </table>
-      </div>
-    </div>
   </div>
   <div class="featured-image-section"> 
       <div class="featured-image-text">Image of the now</div>
-      <img class="featured-img" src="/images/admin/imageofnow.jpg" alt="/images/admin/imageofnow.default.jpg">
+      <img class="featured-img" src="/images/admin/imageofnow.jpg" alt="ERROR: NO IMAGE AVAILABLE">
   </div>
 </div>
 </div>
