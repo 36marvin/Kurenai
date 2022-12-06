fae66ed590ab06772038236f04c957eaa7fb806d
Marvin <dont@show.myemailS>
2022-12-06 18:33:24 -0300

    Sets the anchor element at the footer sub-view to open its link on a new tab and not send any referrer data to GitHub.

--- resources/views/global/footer.blade.php
+++ resources/views/global/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="the-footer">
-    · A texboard running on <a href="https://www.github.com/36marvin/retroboard" class="retro-footer-link">Retroboard</a> ·
+    · A texboard running on <a target="_blank" referrerpolicy="no-referrer" href="https://www.github.com/36marvin/retroboard" class="retro-footer-link">Retroboard</a> ·
     <noscript> 
     <p>Your browser's Javascript is disabled.
     <br>
