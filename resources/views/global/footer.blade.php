d3788423b1caf1457364ac0316a37c4b9ef6603e
Marvin <dont@show.myemailS>
2022-12-06 19:42:06 -0300

    Fixes minor margin issue at the footer sub-view.

--- resources/views/global/footer.blade.php
+++ resources/views/global/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="the-footer">
     · A texboard running on <a target="_blank" referrerpolicy="no-referrer" href="https://www.github.com/36marvin/retroboard" class="retro-footer-link">Retroboard</a> ·
     <noscript> 
-    <p>Your browser's Javascript is disabled.
+    <p class="noscript-footer">Your browser's Javascript is disabled.
     <br>
     You're safer against XSS attacks now, though some non-essential features will be disabled.
     </p>
