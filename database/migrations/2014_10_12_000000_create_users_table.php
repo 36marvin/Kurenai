87b189b4fef4f454ffdb82b6dbec941010d29f28
Marvin <dont@show.myemailS>
2023-02-13 13:14:05 -0300

    Adds email field to the user table. The id should be in the uuid format.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->unique();
+            $table->uuid('id')->unique();
             $table->string('name', 20);
+            $table->string('email', 320);
             $table->string('password', 200); // the hash may be much longer than the original password 
             $table->string('badge_id'); // if any, foreign key
             $table->rememberToken();
