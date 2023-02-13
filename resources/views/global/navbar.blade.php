b83f0fe5f982499ed12a9560f32b29fb1a83d1b4
Marvin <dont@show.myemailS>
2023-02-13 13:21:44 -0300

    The login navbar hyperlink redirects to the login page.
    Also, "lara" who?

--- resources/views/global/navbar.blade.php
+++ resources/views/global/navbar.blade.php
@@ -1,13 +1,13 @@
 <table class="global-navbar">
 <tr>
   <td class="global-navbar__intro global-navbar__td global-navbar__sitename">
-    <div class="site-title"><span class="lara">LARA</span><span class="board">BOARD<span></div>
+    <div class="site-title"><a href="/" class="lara">Kurenai</a></div>
   </td>
   <td class="useful-pages global-navbar__td">
   <div class="global-user-ops__wrapper">
     @if(!Auth::check())
-    <div class="global-user-ops">LOGIN</div>
-    <div class="global-user-ops">SIGN UP</div>
+    <div class="global-user-ops"><a href="/services/login" class="user-ops-link">LOGIN</a></div>
+    <div class="global-user-ops"><a href="/services/signup" class="user-ops-link">SIGN UP</a></div>
     @endif
   </div>
   </td>
