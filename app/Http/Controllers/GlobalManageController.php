40042b1d954deb5541e8c5cdf9cc471699d90d1d
Marvin <dont@show.myemailS>
2023-02-14 17:08:10 -0300

    Creates some empty methods to management controller.
    Config manager is still to be imported later.

--- app/Http/Controllers/GlobalManageController.php
+++ app/Http/Controllers/GlobalManageController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ConfigManagerService;
 
 class GlobalManageController extends Controller
 {
+    private $configManager;
+
+    public function __construct () {
+        $this->configManager = new ConfigManagerService;
+    }
     public function serveGlobalManagementConfigPage () {
         return view('services.manage.global.config');
     }
-}
+
+    public function setKurenaiGeneralConfig() {
+
+    }
+
+    public function setKurenaiPostConfig() {
+
+    }
+
+    public function setKurenaiBoardConfig() {
+
+    }
+}   
